a32e83cc8797535a3f3aa1d96be7f001353f0b3f
Muh Handri Kiswanto <kiswanto.handri@gmail.com>
2025-08-22 21:05:45 +0700

    update conflig

--- splitr-mobile/app/_layout.tsx
+++ splitr-mobile/app/_layout.tsx
@@ -2,11 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
+    
     <Stack
       screenOptions={{
         headerShown: false,
